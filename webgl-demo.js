24541e0b7ac9292c8a7a701f672a88355ec8f4b2
toralarmando <120235604+toralarmando@users.noreply.github.com>
2023-10-14 11:23:49 -0600

    Update webgl-demo.js

--- webgl-demo.js
+++ webgl-demo.js
@@ -85,7 +85,7 @@ function main() {
   const buffers = initBuffers(gl);
 
   // Load texture
-  const texture = loadTexture(gl, "cubetexture.png");
+  const texture = loadTexture(gl, "one.jpeg");
   // Flip image pixels into the bottom-to-top order that WebGL expects.
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 
